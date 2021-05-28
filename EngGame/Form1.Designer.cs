75dbe44a7465295e6a55c90f4dd80711d7d23e54
Александр Кунгурцев <alexander.kungurtcev@gmail.com>
2021-05-28 11:05:37 +0300

    Add files via upload

---
+++ EngGame/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace EngGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ruseng = new System.Windows.Forms.Button();
+            this.Engrus = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.word = new System.Windows.Forms.Label();
+            this.transword = new System.Windows.Forms.TextBox();
+            this.check = new System.Windows.Forms.Button();
+            this.pts = new System.Windows.Forms.Label();
+            this.menu = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Ruseng
+            // 
+            this.Ruseng.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.Ruseng.Location = new System.Drawing.Point(304, 79);
+            this.Ruseng.Name = "Ruseng";
+            this.Ruseng.Size = new System.Drawing.Size(174, 54);
+            this.Ruseng.TabIndex = 0;
+            this.Ruseng.Text = "RusEng";
+            this.Ruseng.UseVisualStyleBackColor = false;
+            this.Ruseng.Click += new System.EventHandler(this.Ruseng_Click);
+            // 
+            // Engrus
+            // 
+            this.Engrus.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.Engrus.Location = new System.Drawing.Point(304, 144);
+            this.Engrus.Name = "Engrus";
+            this.Engrus.Size = new System.Drawing.Size(174, 54);
+            this.Engrus.TabIndex = 1;
+            this.Engrus.Text = "EngRus";
+            this.Engrus.UseVisualStyleBackColor = false;
+            this.Engrus.Click += new System.EventHandler(this.Engrus_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.Exit.Location = new System.Drawing.Point(304, 204);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(174, 54);
+            this.Exit.TabIndex = 2;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Location = new System.Drawing.Point(484, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(304, 346);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // word
+            // 
+            this.word.AutoSize = true;
+            this.word.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.word.Location = new System.Drawing.Point(146, 192);
+            this.word.Name = "word";
+            this.word.Size = new System.Drawing.Size(152, 55);
+            this.word.TabIndex = 4;
+            this.word.Text = "label1";
+            // 
+            // transword
+            // 
+            this.transword.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.transword.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.transword.Location = new System.Drawing.Point(12, 258);
+            this.transword.Name = "transword";
+            this.transword.Size = new System.Drawing.Size(447, 47);
+            this.transword.TabIndex = 5;
+            // 
+            // check
+            // 
+            this.check.Location = new System.Drawing.Point(12, 321);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(447, 54);
+            this.check.TabIndex = 7;
+            this.check.Text = "Проверка";
+            this.check.UseVisualStyleBackColor = true;
+            this.check.Click += new System.EventHandler(this.Check_Click);
+            // 
+            // pts
+            // 
+            this.pts.AutoSize = true;
+            this.pts.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pts.Location = new System.Drawing.Point(109, 12);
+            this.pts.Name = "pts";
+            this.pts.Size = new System.Drawing.Size(100, 37);
+            this.pts.TabIndex = 8;
+            this.pts.Text = "label1";
+            // 
+            // menu
+            // 
+            this.menu.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.menu.Location = new System.Drawing.Point(304, 19);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(174, 54);
+            this.menu.TabIndex = 9;
+            this.menu.Text = "Main Menu";
+            this.menu.UseVisualStyleBackColor = false;
+            this.menu.Click += new System.EventHandler(this.Menu_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(5, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 37);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Очки:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(800, 387);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menu);
+            this.Controls.Add(this.pts);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.transword);
+            this.Controls.Add(this.word);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Engrus);
+            this.Controls.Add(this.Ruseng);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "English Game";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Ruseng;
+        private System.Windows.Forms.Button Engrus;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label word;
+        private System.Windows.Forms.TextBox transword;
+        private System.Windows.Forms.Button check;
+        private System.Windows.Forms.Label pts;
+        private System.Windows.Forms.Button menu;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ EngGame/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace EngGame
+{
+    public partial class Form1 : Form
+    {
+        public int points = 0;
+        public bool reverse = false;
+        public int counter = 1;
+        public int now = 0;
+        public static string[] rus = Properties.Resources.Rus.Split(' ');
+        public static string[] eng = Properties.Resources.Eng.Split(' ');
+        public Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            HideAll();
+            pts.Text = "";
+            word.Text = "";       
+        }
+        private void HideAll()
+        {
+            Ruseng.Show();
+            Engrus.Show();
+            Exit.Show();
+            menu.Hide();
+            pictureBox1.Hide();
+            pts.Hide();
+            word.Hide();
+            transword.Hide();
+            check.Hide();
+            label1.Hide();
+        }
+        private void ShowAll()
+        {
+            label1.Show();
+            menu.Show();
+            pictureBox1.Show();
+            pts.Show();
+            word.Show();
+            transword.Show();
+            check.Show();
+            Engrus.Hide();
+            Ruseng.Hide();
+            Exit.Hide();
+        }
+
+        private void Ruseng_Click(object sender, EventArgs e)
+        {
+                ShowAll();
+                reverse = false;
+                MainGame();
+        }       
+
+        private void Engrus_Click(object sender, EventArgs e)
+        {
+                ShowAll();
+                reverse = true;
+                MainGame();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MainGame()
+        {
+            pts.Text = points.ToString();
+            if (counter == 1)
+            { pictureBox1.Image = new Bitmap(Properties.Resources._1); }
+            if (!reverse)
+            {
+                now = rnd.Next(rus.Length);
+                word.Text = rus[now];
+            }
+            else
+            {   
+                now = rnd.Next(eng.Length);
+                word.Text = eng[now]; 
+            }
+        }
+
+        private void Check_Click(object sender, EventArgs e)
+        {
+            if (!reverse)
+            {
+                if (transword.Text == eng[now])
+                {
+                    points++;
+                }
+                else
+                {
+                    counter++;
+                    NewImg();
+                }                
+            }
+            else
+            {
+                if (transword.Text == rus[now])
+                {
+                    points++;
+                }
+                else
+                {
+                    counter++;
+                    NewImg();
+                }
+            }
+            transword.Text = "";
+            MainGame();
+
+        }
+
+        private void NewImg()
+        {
+            switch (counter)
+            {
+                case 2: { pictureBox1.Image = new Bitmap(Properties.Resources._2); } break;
+                case 3: { pictureBox1.Image = new Bitmap(Properties.Resources._3); } break;
+                case 4: { pictureBox1.Image = new Bitmap(Properties.Resources._4); } break;
+                case 5: { pictureBox1.Image = new Bitmap(Properties.Resources._5); } break;
+                case 6: { pictureBox1.Image = new Bitmap(Properties.Resources._6); } break;
+                case 7: { pictureBox1.Image = new Bitmap(Properties.Resources._7); Lose(); } break;
+            }
+        }
+
+        private void Lose()
+        {         
+            HideAll();
+            MessageBox.Show("You have "+points.ToString()+ " points!", "Сообщение");
+            pictureBox1.Image = new Bitmap(Properties.Resources._1);
+            counter = 0;
+            points = 0;
+
+        }
+
+        private void Menu_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(Properties.Resources._1);
+            counter = 0;
+            points = 0;
+            HideAll();
+        }
+    }
+}
+
